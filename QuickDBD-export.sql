7cfd20c5dd7bf23963c7d9a6d73ce355c00baca6
Sarah Schilling <149340920+SarahSchilling618@users.noreply.github.com>
2024-06-04 20:51:40 -0400

    Add files via upload

---
+++ QuickDBD-export.sql
@@ -0,0 +1,31 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "dementia_patients_health_data" (
+    "Diabetic" INT   NOT NULL,
+    "AlcoholLevel" FLOAT   NOT NULL,
+    "HeartRate" INT   NOT NULL,
+    "BloodOxygenLevel" FLOAT   NOT NULL,
+    "BodyTemperature" FLOAT   NOT NULL,
+    "Weight" FLOAT   NOT NULL,
+    "MRI_Delay" FLOAT   NOT NULL,
+    "Prescription" VARCHAR(225)   NOT NULL,
+    "Dosage_in_mg" FLOAT   NOT NULL,
+    "Age" INT   NOT NULL,
+    "Education_Level" VARCHAR(225)   NOT NULL,
+    "Dominant_Hand" VARCHAR(225)   NOT NULL,
+    "Gender" VARCHAR(225)   NOT NULL,
+    "Family_History" VARCHAR(225)   NOT NULL,
+    "Smoking_Status" VARCHAR(225)   NOT NULL,
+    "APOE_ε4" VARCHAR(225)   NOT NULL,
+    "Physical_Activity" VARCHAR(225)   NOT NULL,
+    "Depression_Status" VARCHAR(225)   NOT NULL,
+    "Cognitive_Test_Scores" INT   NOT NULL,
+    "Medication_History" VARCHAR(225)   NOT NULL,
+    "Nutrition_Diet" VARCHAR(225)   NOT NULL,
+    "Sleep_Quality" VARCHAR(225)   NOT NULL,
+    "Chronic_Health_Conditions" VARCHAR(225)   NOT NULL,
+    "Dementia" INT   NOT NULL
+);
+
